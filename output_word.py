863b013d85d2d2ba406fff0c97c2f230a6be8e01
appleyuta <appleappleyuta@gmail.com>
2022-11-02 13:51:47 +0900

    spec7: 単語に読みがなを関連付ける機能を追加

--- output_word.py
+++ output_word.py
@@ -3,16 +3,16 @@ import sys
 import json
 
 #辞書データの読み込み
-with open('./word_data.json') as f:
+with open('./word_data.json', "r", encoding="utf-8") as f:
     dic_data = json.load(f)
 
 #引数の数によって挙動を変更する
 if len(sys.argv) == 2:
     #引数で指定されたidの単語を出力
     word_data = dic_data[int(sys.argv[1])]
-    print(str(word_data["id"])+": " + word_data["word"])
+    print(f"{word_data['id']}: {word_data['word']} {word_data['kana']}")
 
 else:
     #辞書データに登録されているidと単語をすべて出力
     for word_data in dic_data:
-        print(str(word_data["id"])+": " + word_data["word"])
+        print(f"{word_data['id']}: {word_data['word']} {word_data['kana']}")

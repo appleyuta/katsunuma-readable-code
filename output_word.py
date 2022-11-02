f083f360946eebe758ad71844da4dcbfa78e711e
appleyuta <appleappleyuta@gmail.com>
2022-11-02 14:27:00 +0900

    spec8: ユーザーの名前を関連付けて出力する機能を追加

--- output_word.py
+++ output_word.py
@@ -6,13 +6,24 @@ import json
 with open('./word_data.json', "r", encoding="utf-8") as f:
     dic_data = json.load(f)
 
+
+# ユーザ情報を取得
+userdata = dic_data['userdata']
+
+# 単語情報を取得
+word_info = dic_data['word_info']
+
+# ユーザ情報の表示
+print(f"ユーザー名: {userdata['username']}")
+
+
 #引数の数によって挙動を変更する
 if len(sys.argv) == 2:
     #引数で指定されたidの単語を出力
-    word_data = dic_data[int(sys.argv[1])]
+    word_data = word_info[int(sys.argv[1])]
     print(f"{word_data['id']}: {word_data['word']} {word_data['kana']}")
 
 else:
     #辞書データに登録されているidと単語をすべて出力
-    for word_data in dic_data:
+    for word_data in word_info:
         print(f"{word_data['id']}: {word_data['word']} {word_data['kana']}")
